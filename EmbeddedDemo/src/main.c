65bc9c576b33b0abc44152723ad81ce51dc8332f
Craig Sacco <craig.sacco@gmail.com>
2018-05-20 15:25:28 +1000

    now have a firmware blobgit status!

--- EmbeddedDemo/src/main.c
+++ EmbeddedDemo/src/main.c
@@ -30,10 +30,29 @@ SOFTWARE.
 /* Includes */
 #include "stm32f4xx.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
+
 /* Private macro */
 /* Private variables */
 /* Private function prototypes */
 /* Private functions */
+
+void vApplicationTickHook( void )
+{
+}
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+	( void ) pcTaskName;
+	( void ) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
 
 /**
 **===========================================================================
